86ba5ee2e6f87e1a224389a1730cb69a22824930
TheBrainGamingSingh <manpreetsingh10399@gmail.com>
2019-11-13 21:00:32 +0530

    login works

--- usfpes.py
+++ usfpes.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template,url_for
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'adab767d91bbb625b9b8a3d637802419'
 @app.route("/")
 
 @app.route("/home")
@@ -11,6 +13,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+    
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
