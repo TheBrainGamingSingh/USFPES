65d498d92fb302bbb095a7cdd263ee750d98646f
TheBrainGamingSingh <manpreetsingh10399@gmail.com>
2019-11-14 16:51:23 +0530

    added gitignore

--- usfpes/routes.py
+++ usfpes/routes.py
@@ -1,3 +1,5 @@
+import os
+import secrets
 from flask import render_template, url_for, flash, redirect, request
 from usfpes import app,db,bcrypt
 from usfpes.models import User
@@ -52,12 +54,25 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
+
 
 @app.route("/account",methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+            
         current_user.username = form.username.data
         current_user.email = form.email.data
         current_user.student_id = form.student_id.data
@@ -67,7 +82,7 @@ def account():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-        form.student_id.data = current_user.student_id      
+        form.student_id.data = current_user.student_id
 
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file,form=form)
