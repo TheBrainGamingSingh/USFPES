6699053bf21b8d6ca3d15c4b98dc2521809ca934
TheBrainGamingSingh <manpreetsingh10399@gmail.com>
2019-11-14 23:36:42 +0530

    added environment variables

--- usfpes/routes.py
+++ usfpes/routes.py
@@ -97,7 +97,7 @@ def account():
 
 def send_reset_email(user):
     token = user.get_reset_token()
-    msg = Message('Password Reset Request',sender='#',recipients=[user.email])
+    msg = Message('Password Reset Request',sender='usfpes@gmail.com',recipients=[user.email])
     msg.body = f''' To reset your password, visit the following link:
 {url_for('reset_token',token=token,_external=True)}
 
